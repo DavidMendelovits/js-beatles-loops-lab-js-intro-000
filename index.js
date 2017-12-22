1668344825cb407a91a15411a9c98547cd150d79
David Mendelovits <david.mendelovits@gmail.com>
2017-12-22 22:31:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ function theBeatlesPlay (musicians, instruments) {
   return allMusicians;
 }
 
-function johnLennonFacts([]) {
-  var facts =
-  ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+const facts =
+["He was the last Beatle to learn to drive", 
+"He was never a vegetarian", 
+"He was a choir boy and boy scout", 
+"He hated the sound of his own voice"];
 
+function johnLennonFacts(facts) {
   var allFacts = [];
   var i = 0;
   while (i < facts.length) {
