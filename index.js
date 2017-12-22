1afb82666c0fb68047cd58c768830eb65ba870fe
David Mendelovits <david.mendelovits@gmail.com>
2017-12-22 22:39:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles (x) {
   var array = []:
   do {
-    x = array.push("I love the Beatles");
+    x = array.push("I love the Beatles!");
     x++;
   } while (x < 15); {
 
